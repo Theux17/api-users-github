cfeda260a5cb4b440934846aca98ab6695b4808d
Theux17 <oliveira.ms18@gmail.com>
2020-11-20 14:35:59 -0300

    Criando a tabela de usuários

---
+++ src/database/migrations/20201120151618-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', { 
+      id:{ 
+        type: Sequelize.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name:{ 
+        type: Sequelize.TEXT, 
+        allowNull: false
+      },
+      location: {
+        type: Sequelize.TEXT
+      },
+      avatar: {
+        type: Sequelize.TEXT
+      },
+      username: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      biography: {
+        type: Sequelize.TEXT
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
