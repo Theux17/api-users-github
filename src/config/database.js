d02dd5fe33eac38092bfe6d06ba31bda7bac5e4a
Theux17 <oliveira.ms18@gmail.com>
2020-11-20 12:11:27 -0300

    Configurando conexão com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'puyolsilva17',
+    database: 'github',
+    define: {
+        underscored: true
+    }
+}
